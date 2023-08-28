2d3d3f77dba0682790069fa1ee51e47810a46206
HardickRaj <rajhardick1@gmail.com>
2023-08-28 17:56:01 +0530

    added all the file, i.e. configs

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'hero-bg':"url('/assets/images/mars-1.jpg')"
+      },
+      fontFamily:{
+        'rajdhani':['Rajdhani', 'sans-serif'],
+        'bitter':['Bitter', 'serif']
+      },
+      colors: {
+        'primary-bg':'#161623',
+        'secondary-bg':'#1f2c39',
+      },
+    },
+  },
+  plugins: [],
+}
